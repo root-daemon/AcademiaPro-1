6a595e298c57de0ed251b0533b490ba3c900ce90
Rahuletto <rahulmarban@gmail.com>
2025-02-14 13:37:03 +0530

    feat: url directory

--- hooks/fetchFiles.tsx
+++ hooks/fetchFiles.tsx
@@ -1,28 +1,10 @@
 "use server";
 import { cache } from "react";
-import { cookies } from "next/headers";
-import { filesUrl } from "@/utils/URL";
-import { CoursePapers } from "@/types/CoursePapers";
+import { files } from "@/library/files";
 
 
 async function fetchFiles() {
-    const cookie = (await cookies()).get("key");
-
-    const response = await fetch(`${filesUrl}/files`, {
-        method: "GET",
-        cache: "force-cache",
-        next: {
-            revalidate: 3600,
-        },
-        headers: {
-            "Content-Type": "application/json",
-            "X-CSRF-Token": cookie?.value ?? "",
-        },
-    });
-
-    const json: CoursePapers[] = await response.json();
-
-    return json;
+    return files;
 }
 
 export const fetchFileArray = cache(async () => {
--- hooks/fetchResources.tsx
+++ hooks/fetchResources.tsx
@@ -1,28 +1,10 @@
 "use server";
 import { cache } from "react";
-import { cookies } from "next/headers";
-import { filesUrl } from "@/utils/URL";
-import { Folders } from "@/types/Folders";
+import { resourcesFiles } from "@/library/resources";
 
 
 async function fetchResources() {
-    const cookie = (await cookies()).get("key");
-
-    const response = await fetch(`${filesUrl}/resources`, {
-        method: "GET",
-        cache: "force-cache",
-        next: {
-            revalidate: 3600,
-        },
-        headers: {
-            "Content-Type": "application/json",
-            "X-CSRF-Token": cookie?.value ?? "",
-        },
-    });
-
-    const json: Folders = await response.json();
-
-    return json;
+    return resourcesFiles;
 }
 
 export const fetchResourcesArray = cache(async () => {
