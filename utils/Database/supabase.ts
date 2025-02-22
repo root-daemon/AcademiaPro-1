c9fdfc76fecac4340bdf2fe681b5bb6dcfe02063
Rahuletto <rahulmarban@gmail.com>
2025-02-22 14:36:33 +0530

    fix: url

--- utils/Database/supabase.ts
+++ utils/Database/supabase.ts
@@ -5,6 +5,6 @@ const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const SUPABASE_KEY = process.env.NEXT_PUBLIC_SERVICE_KEY;
 
 // Create a Supabase client instance
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY ?? "");
+const supabase = createClient(SUPABASE_URL ?? "", SUPABASE_KEY ?? "");
 
 export { supabase };
