0bed19f26eeee3f734f5e6cf7d3bd6550c86560e
Rahul Marban <rahulmarban@gmail.com>
2025-04-18 23:14:24 +0530

    fix: type errors

--- app/academia/components/Marks/Card/MarkElement.tsx
+++ app/academia/components/Marks/Card/MarkElement.tsx
@@ -11,10 +11,10 @@ export default function MarkElement({ test }: { test: TestPerformance }) {
 }
 
 export function MarkDisplay({ marks }: { marks: Marks & Overall }) {
-	const scored = marks.scored ? marks.scored : marks.marks;
+	const scored = marks.scored
 	return (
 		<div
-			className={`${Number(scored) === Number(marks.total) && !marks.marks ? "border-light-success-color dark:border-dark-success-color" : Number(scored) < Number(marks.total) / 2 && !marks.marks ? "border-light-error-color dark:border-dark-error-color border-dashed" : "border-transparent"} flex items-center gap-1 rounded-full border bg-light-background-dark dark:bg-dark-background-dark`}
+			className={`${Number(scored) === Number(marks.total) && !scored ? "border-light-success-color dark:border-dark-success-color" : Number(scored) < Number(marks.total) / 2 && !scored ? "border-light-error-color dark:border-dark-error-color border-dashed" : "border-transparent"} flex items-center gap-1 rounded-full border bg-light-background-dark dark:bg-dark-background-dark`}
 		>
 			<span
 				className={`px-2 pr-1 text-sm font-medium ${scored == "Abs" || Number(scored) < Number(marks.total) / 2 ? "text-light-error-color dark:text-dark-error-color" : Number(scored) === Number(marks.total) ? "text-light-success-color dark:text-dark-success-color" : "text-light-color dark:text-dark-color"}`}
@@ -22,9 +22,9 @@ export function MarkDisplay({ marks }: { marks: Marks & Overall }) {
 				{scored}
 			</span>
 			<span
-				className={`${marks.marks ? "bg-light-success-background text-light-success-color dark:bg-dark-success-background dark:text-dark-success-color" : "bg-light-color text-light-background-light dark:bg-dark-color dark:text-dark-background-darker"} rounded-full px-2 py-0.5 text-sm font-bold opacity-80`}
+				className={`${scored ? "bg-light-success-background text-light-success-color dark:bg-dark-success-background dark:text-dark-success-color" : "bg-light-color text-light-background-light dark:bg-dark-color dark:text-dark-background-darker"} rounded-full px-2 py-0.5 text-sm font-bold opacity-80`}
 			>
-				{marks.marks ? marks.total.split(".")[0] : marks.total}
+				{scored ? marks.total.split(".")[0] : marks.total}
 			</span>
 		</div>
 	);
